c213e271fd82574410ca5a9baac5433d60bbf0ed
You <you@example.com>
2024-09-15 17:09:32 +0700

    update config

--- client/config.js
+++ client/config.js
@@ -1,7 +1,8 @@
 const apiId = 'ygq9gyji0b'
-const apiEndpoint = `https://ygq9gyji0b.execute-api.us-east-1.amazonaws.com/dev`
-const authConfig = {
-  domain: 'dev-05hqpn4sprwg81vs.us.auth0.com',
-  clientId: '26El3WAvCg5ClBLjyRUKRNTjP8mmTJPc',
+export const apiEndpoint = `https://ygq9gyji0b.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-05hqpn4sprwg81vs.us.auth0.com',    // Domain from Auth0
+  clientId: '26El3WAvCg5ClBLjyRUKRNTjP8mmTJPc',  // Client id from an Auth0 application
   callbackUrl: 'http://localhost:3000/callback'
 }
